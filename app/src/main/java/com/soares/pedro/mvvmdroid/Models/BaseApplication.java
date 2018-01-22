db06d531cae0bea20a86eec9921094f6a92a67b4
Pedro Soares <pedro.dcarmo.soares@outlook.com>
2018-01-22 18:39:23 +0000

    added BaseApplication

---
+++ app/src/main/java/com/soares/pedro/mvvmdroid/Models/BaseApplication.java
@@ -0,0 +1,33 @@
+package com.soares.pedro.mvvmdroid.Models;
+
+import android.app.Application;
+
+import com.soares.pedro.mvvmdroid.Services.CurrentActivityService;
+import com.soares.pedro.mvvmdroid.Services.DialogService;
+import com.soares.pedro.mvvmdroid.Services.Interfaces.ICurrentActivityService;
+import com.soares.pedro.mvvmdroid.Services.Interfaces.IDialogService;
+import com.soares.pedro.mvvmdroid.Services.Interfaces.INavigationService;
+import com.soares.pedro.mvvmdroid.Services.NavigationService;
+import com.soares.pedro.mvvmdroid.Services.ServiceLocator;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ServiceLocator.getInstance().registerService(ICurrentActivityService.class, CurrentActivityService.class);
+        ServiceLocator.getInstance().registerService(INavigationService.class, NavigationService.class);
+        ServiceLocator.getInstance().registerService(IDialogService.class, DialogService.class);
+
+
+        ICurrentActivityService currentActivityService = ServiceLocator.getInstance().getService(ICurrentActivityService.class);
+        registerActivityLifecycleCallbacks(currentActivityService.getActivityMonitor());
+    }
+
+    @Override
+    public void onTerminate() {
+        ICurrentActivityService currentActivityService = ServiceLocator.getInstance().getService(ICurrentActivityService.class);
+        unregisterActivityLifecycleCallbacks(currentActivityService.getActivityMonitor());
+        super.onTerminate();
+    }
+}

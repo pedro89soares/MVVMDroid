57fb6731b730146dec6ba9b8909cd5f0cbe6746c
Pedro Soares <pedro.dcarmo.soares@outlook.com>
2018-01-22 18:57:17 +0000

    added IViewLocatorService to baseApplication

--- app/src/main/java/com/soares/pedro/mvvmdroid/Models/BaseApplication.java
+++ app/src/main/java/com/soares/pedro/mvvmdroid/Models/BaseApplication.java
@@ -7,8 +7,10 @@ import com.soares.pedro.mvvmdroid.Services.DialogService;
 import com.soares.pedro.mvvmdroid.Services.Interfaces.ICurrentActivityService;
 import com.soares.pedro.mvvmdroid.Services.Interfaces.IDialogService;
 import com.soares.pedro.mvvmdroid.Services.Interfaces.INavigationService;
+import com.soares.pedro.mvvmdroid.Services.Interfaces.IViewLocatorService;
 import com.soares.pedro.mvvmdroid.Services.NavigationService;
 import com.soares.pedro.mvvmdroid.Services.ServiceLocator;
+import com.soares.pedro.mvvmdroid.Services.ViewLocatorService;
 
 public class BaseApplication extends Application {
 
@@ -18,6 +20,7 @@ public class BaseApplication extends Application {
         ServiceLocator.getInstance().registerService(ICurrentActivityService.class, CurrentActivityService.class);
         ServiceLocator.getInstance().registerService(INavigationService.class, NavigationService.class);
         ServiceLocator.getInstance().registerService(IDialogService.class, DialogService.class);
+        ServiceLocator.getInstance().registerService(IViewLocatorService.class, ViewLocatorService.class);
 
 
         ICurrentActivityService currentActivityService = ServiceLocator.getInstance().getService(ICurrentActivityService.class);

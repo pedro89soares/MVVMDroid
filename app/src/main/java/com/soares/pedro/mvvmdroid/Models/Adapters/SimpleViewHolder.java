2efb56b0da175592723e0c136b1f64933d28306f
Pedro Soares <pedro89soares@gmail.com>
2018-03-13 16:51:38 +0000

    added RecyclerAdapter and Manifest Changed

---
+++ app/src/main/java/com/soares/pedro/mvvmdroid/Models/Adapters/SimpleViewHolder.java
@@ -0,0 +1,10 @@
+package com.soares.pedro.mvvmdroid.Models.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class SimpleViewHolder extends RecyclerView.ViewHolder {
+    public SimpleViewHolder(View itemView) {
+        super(itemView);
+    }
+}

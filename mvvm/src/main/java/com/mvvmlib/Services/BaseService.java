3288dde3af73ec747b1cef178cdda960344aebd0
Pedro Soares <psoares@agap2.pt>
2018-05-21 14:59:16 +0100

    Application Context added to BaseService

--- mvvm/src/main/java/com/mvvmlib/Services/BaseService.java
+++ mvvm/src/main/java/com/mvvmlib/Services/BaseService.java
@@ -1,15 +1,24 @@
 package com.mvvmlib.Services;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mvvmlib.Services.Interfaces.ICurrentActivityService;
 
 public class BaseService {
 
+    private Context context;
+
     protected AppCompatActivity getCurrentActivity() {
         ICurrentActivityService activityService = ServiceLocator.getInstance().getService(ICurrentActivityService.class);
         if (activityService != null)
             return activityService.getCurrentActivity();
         return null;
+    }
+
+    protected Context getContext(){
+        if(context==null && getCurrentActivity()!=null)
+            context = getCurrentActivity().getApplicationContext();
+        return context;
     }
 }
